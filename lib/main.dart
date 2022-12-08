f8a2fb4234b98ae3285754fbc12184db269f4dfd
salemgu1 <74470511+salemgu1@users.noreply.github.com>
2022-12-08 09:08:08 +0200

    Start project
    Start project with flutter

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final keyApplicationId = 'px8btqBuwCdksgl8O0JGSLrX8i066M0k1fpLYag8';
+  final keyClientKey = '1Kw9ryhNNx8HzqLQSa6fQLgq88tr0y607s2vN6PP';
+  final keyParseServerUrl = 'https://parseapi.back4app.com';
+
+  await Parse().initialize(keyApplicationId, keyParseServerUrl,
+      clientKey: keyClientKey, autoSendSessionId: true);
+
+  runApp(MaterialApp(
+      home: MyApp()
+  ));
+}
+
+class App extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter SignUp',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Signup(),
+    );
+  }
+}
